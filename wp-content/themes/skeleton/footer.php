2fbd866ce38059a44e5463c5cc9bdfdc1825ee64
Matt <matt@mspacecreative.com>
2019-01-18 08:26:36 -0400

    c

---
+++ wp-content/themes/skeleton/footer.php
@@ -0,0 +1,8 @@
+<?php st_before_footer();?>
+
+
+
+<?php st_after_footer();?>
+
+</body>
+</html>
---
+++ wp-content/themes/skeleton/small_banner.php
@@ -0,0 +1,3 @@
+<?php
+echo '<img src="http://heattheatre.com/site/wp-content/uploads/2013/04/small_banner.jpg">';
+?>
